9c342abb2df3c659c3fe7cbfcd8f9b9cea8151ea
aplam <aplam@buffalo.edu>
2019-04-08 15:00:21 -0400

    Testing height.
    Wrote a height adjustment script and programmed a button to increase height. However getting this to work on anything other than the main menu is not finished. Further testing is required.

---
+++ Educational Game For VR/Assets/Scenes/Player_height.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_height : MonoBehaviour
+{
+	private float currentHeight = 1.0f;
+
+	[SerializeField]
+	private Camera VRCamera;
+
+	public void ResizeUp()
+	{
+		currentHeight = currentHeight + 0.1f;
+		transform.localScale = new Vector3(1, currentHeight, 1);
+	}
+
+	public void ResizeDown()
+	{
+		currentHeight = currentHeight - 0.1f;
+		transform.localScale = new Vector3(1, currentHeight, 1);
+	}
+
+}
