300f2223abc8c02112e6c39e4c5fd215e7a064a8
ALPELA <Solidcrate101@gmail.com>
2019-04-12 01:59:31 -0400

    Unit Test added
    Added Unit test for height that tests both if the camera goes up and down properly. Done in Unity's test runner.

---
+++ Educational Game For VR/Assets/Scenes/Tests/Height test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Heighttest
+    {
+		[Test]
+		public void ResizeUpTest()
+		{
+			float currentHeight = 1.0f;
+			currentHeight = currentHeight + 0.1f;
+			Vector3 height = new Vector3(1, currentHeight, 1);
+			Assert.AreEqual(1.1f, height.y);
+
+		}
+
+		[Test]
+		public void ResizeDownTest()
+		{
+
+			float currentHeight = 1.0f;
+			currentHeight = currentHeight - 0.1f;
+			Vector3 height = new Vector3(1, currentHeight, 1);
+			Assert.AreEqual(0.9f,  height.y);
+
+		}
+	}
+}
