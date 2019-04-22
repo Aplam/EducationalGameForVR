97e84fe9a5204095f4eb5888995b994c683473e5
DonaldMB <don.m.brant@gmail.com>
2019-04-22 14:00:02 -0400

    Initial layout of Flaschard Level scripting
    I've layed the ground work for the scripting layout of the Flashcard Level with FlashcardProgressionTracker.cs and LevelFlashcard.cs. These scripts have been added to the scene as components on the player.

---
+++ Educational Game For VR/Assets/Scripts/Level - Flashcard/FlashcardProgressionTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashcardProgressionTracker : MonoBehaviour {
+  // the worldspace Text objects where flashcard text will be displayed to the player
+  public Text cardFront;
+  public Text cardBack;
+  // progressAudio: the Audio Source object that will indicate to the player that a step has been completed
+  public AudioSource progressAudio;
+  // level: The Level object associated with the current level
+  public LevelFlashcard level;
+  // stepCount: The current step the player is on (0 indexed, 0 = 'Step 1')
+  private int stepCount;
+  // Start is called before the first frame update
+  void Start() {
+    stepCount = 0;
+    cardFront.text = level.frontText[stepCount];
+    cardBack.text = level.backText[stepCount];
+  }
+  // Call NextStep once a step is completed to update the stepCount and show the player the next step
+  void NextStep() {
+    if(stepCount < level.frontText.Length - 1) {
+      stepCount += 1;
+      cardFront.text = level.frontText[stepCount];
+      cardBack.text = level.backText[stepCount];
+      progressAudio.Play();
+    }
+  }
+  // Update is called once per frame
+  void Update() {
+    if(level.stepCompleted()) {
+      NextStep();
+    }
+  }
+}
---
+++ Educational Game For VR/Assets/Scripts/Level - Flashcard/LevelFlashcard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using System.IO;
+
+// Level - Progrssion Tracking
+
+// This class is intended to provide methods for returning level specific data
+// EACH LEVEL SHOULD HAVE ITS OWN UNIQUE Level.cs
+
+public class LevelFlashcard : MonoBehaviour
+{
+  public string[] frontText;
+  public string[] backText;
+  // Start is called before the first frame update
+  void Start() {
+    // TODO: Read in frontText and backText values from CSV
+  }
+
+  public bool stepCompleted() {
+    return SteamVR_Actions._default.TopButton.GetStateUp(SteamVR_Input_Sources.Any);
+  }
+
+// Update is called once per frame
+  void Update() {
+
+  }
+}
