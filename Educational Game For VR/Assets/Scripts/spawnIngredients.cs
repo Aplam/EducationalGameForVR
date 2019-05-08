720148dff3c91503834c967d7b1ee0d882f6eab7
JOSE\jr607 <joseromero611@gmail.com>
2019-05-08 15:08:51 -0400

    you can make a sandwich!
    Added breadslice, cheeseslice, tomatoslice, lettuceslice, and hamslice along with their spawning buttons

---
+++ Educational Game For VR/Assets/Scripts/spawnIngredients.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnIngredients : MonoBehaviour
+{
+	public Transform spawnPos;
+	public GameObject breadToSpawn;
+    public GameObject tomatoToSpawn;
+    public GameObject cheeseToSpawn;
+    public GameObject hamToSpawn;
+    public GameObject lettuceToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        breadToSpawn = GameObject.Find("breadslice");
+        tomatoToSpawn = GameObject.Find("tomato");
+        cheeseToSpawn = GameObject.Find("cheese");
+        hamToSpawn = GameObject.Find("ham");
+        lettuceToSpawn = GameObject.Find("lettuce");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void dropBread(){
+        Instantiate(breadToSpawn, spawnPos.position, spawnPos.rotation);
+    }
+
+    public void dropTomato(){
+		Instantiate(tomatoToSpawn, spawnPos.position, spawnPos.rotation);
+	}
+
+    public void dropCheese()
+    {
+        Instantiate(cheeseToSpawn, spawnPos.position, spawnPos.rotation);
+    }
+
+    public void dropHam()
+    {
+        Instantiate(hamToSpawn, spawnPos.position, spawnPos.rotation);
+    }
+
+    public void dropLettuce()
+    {
+        Instantiate(lettuceToSpawn, spawnPos.position, spawnPos.rotation);
+    }
+}
