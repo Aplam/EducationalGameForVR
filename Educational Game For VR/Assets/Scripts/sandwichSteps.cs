0809547d385aa84854a1e43f893138ee13b53200
JOSE\jr607 <joseromero611@gmail.com>
2019-05-10 05:36:36 -0400

    Added sandwich recipe, no detection
    -sandwich recipe
    -no detection yet, will do in school
    -fixed buttons overlapping bug
    -IF THERE IS MERGE CONFLICT WITH PROGRESSION, IGNORE MY CHANGES

---
+++ Educational Game For VR/Assets/Scripts/sandwichSteps.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+// sandwich - Progrssion Tracking
+
+// This class is intended to provide methods for returning level specific data
+// EACH LEVEL SHOULD HAVE ITS OWN UNIQUE Level.cs
+
+public class sandwichSteps : MonoBehaviour
+{
+    public string[] instruct;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instruct = new string[]{
+        "Step 1: Retrieve the bread and place it on the gray cutting board.\n",
+        "Step 2: Retrieve the ham and place it on the cutting board. \n",
+        "Step 3: Retrieve the cheese and place it on the cutting board.\n",
+        "Step 4: Retrieve the tomato and place it on the board.\n",
+        "Step 5: Retrieve the lettuce and place it on the board.\n",
+        "Congratulations! A sandwich is yours!"
+         };
+    }
+
+    public string getInstruct(int i)
+    {
+        return instruct[i];
+    }
+    public bool stepCompleted()
+    {
+        return SteamVR_Actions._default.TopButton.GetStateUp(SteamVR_Input_Sources.Any);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Educational Game For VR/Assets/Scripts/sandwichTracker.cs
+++ Educational Game For VR/Assets/Scripts/sandwichTracker.cs
@@ -2,32 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using sandwichSteps;
 
-public class ProgressionTracker : MonoBehaviour {
+public class sandwichTracker : MonoBehaviour {
   // stepText: the worldspace Text object where instructions will be displayed to the player
   public Text stepText;
+
   // progressAudio: the Audio Source object that will indicate to the player that a step has been completed
   public AudioSource progressAudio;
+
   // level: The Level object associated with the current level
-  public Level level;
+  public sandwichSteps sandwich;
+
   // stepCount: The current step the player is on (0 indexed, 0 = 'Step 1')
   private int stepCount;
+
   // Start is called before the first frame update
   void Start() {
     stepCount = 0;
-    stepText.text = level.instructions[stepCount];
+    stepText.text = sandwich.getInstruct(stepCount);
   }
+
   // Call NextStep once a step is completed to update the stepCount and show the player the next step
   void NextStep() {
-    if(stepCount < level.instructions.Length - 1) {
+    if(stepCount < sandwich.instruct.Length - 1) {
       stepCount += 1;
-      stepText.text += level.instructions[stepCount];
+      stepText.text += sandwich.instruct[stepCount];
       progressAudio.Play();
     }
   }
+    
   // Update is called once per frame
   void Update() {
-    if(level.stepCompleted()) {
+    if(sandwich.stepCompleted()) {
       NextStep();
     }
   }
